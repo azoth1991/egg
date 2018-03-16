99b2d4510defbdb7f7049243ee6ebd24a38b3a36
azoth <zhou.vip@icloud.com>
2018-03-16 22:49:33 +0800

    getuserinfo

--- wx/app/controller/home.js
+++ wx/app/controller/home.js
@@ -1,12 +1,24 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-// var WechatAPI = require('co-wechat-api');
+const getToken = require('../websdk/getWebToken');
+const getUserInfo = require('../websdk/getWebUserInfo');
 var config = require('../config');
 class HomeController extends Controller {
   async index() {
-    // var api = new WechatAPI(config.appId, config.appSecret);
-    this.ctx.body = 'hi, egg';
+    var query = this.ctx.query;
+    var ctx = this.ctx;
+    // this.ctx.body = 'hi'
+    // getToken(query.code)
+    getToken('071685Tg2Q8kZH0kmsTg2aMNSg2685Tn')
+    .then(function (data) {
+      return JSON.parse(data);
+    })
+    .then(function (data) {
+      getUserInfo(data['access_token'], data['openid']).then(_ => {
+        ctx.body = JSON.stringify({userinfo: _});
+      })
+    });
   }
 }
 
