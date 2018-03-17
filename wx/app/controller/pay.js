7906a5b49ee7c6e86aa8d05d151a51761a1dfea4
azoth <zhou.vip@icloud.com>
2018-03-17 20:02:22 +0800

    pay

--- wx/app/controller/pay.js
+++ wx/app/controller/pay.js
@@ -23,14 +23,14 @@ const tenPayconfig = {
   // spbill_create_ip: 'IP地址'
 };
 const api = new tenpay(tenPayconfig);
-
-
 class PayController extends Controller {
   async index() {
     let res = await api.getPayParamsByPrepay({
       prepay_id: '123'
     });
+    // this.ctx.body = res;
     console.log(res);
+    await this.ctx.render('pay/pay.tpl',{...res});
   }
 }
 
--- wx/config/config.default.js
+++ wx/config/config.default.js
@@ -1,11 +1,17 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = exports = {
+    view: {
+      defaultViewEngine: 'nunjucks',
+      mapping: {
+        '.tpl': 'nunjucks',
+      },
+    },
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1521125368833_4992';
-
   // add your config here
   config.middleware = [];
 
