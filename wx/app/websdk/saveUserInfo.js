8de07e5a307e640d23458bf982d172a62f4e7cf3
azoth <zhou.vip@icloud.com>
2018-03-25 16:25:18 +0800

    cuntry

--- wx/app/websdk/saveUserInfo.js
+++ wx/app/websdk/saveUserInfo.js
@@ -4,7 +4,7 @@ const qs = require('querystring');
 const config = require('../config');
 
 function saveUserInfo(info) {
-  let reqUrl = `http://101.37.151.85:8080/socket/regUser?openid=${info['openid']}&nickname=${info['nickname']}&sex=${info['sex']}&language=${info['language']}&city=${info['city']}&province=${info['province']}&headimgurl=${info['headimgurl']}`;
+  let reqUrl = `http://101.37.151.85:8080/socket/regUser?openid=${info['openid']}&nickname=${info['nickname']}&sex=${info['sex']}&language=${info['language']}&city=${info['city']}&province=${info['province']}&headimgurl=${info['headimgurl']}&country=${info['country']}`;
   let options = {
     method: 'get',
     url: reqUrl
